30c2e3b06799d0a1f917b433168550c90f60d979
timzo31 <madty31@gmail.com>
2022-08-10 02:40:35 +0800

    1-NODE-FARM: HTML Templating: Parsing Variables From URLs, done.

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -40,16 +40,17 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  //const pathName = req.url;
 
   // Overview Page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
-    console.log(cardsHtml);
+    //console.log(cardsHtml);
 
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
 
@@ -59,13 +60,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product Page
-  } else if (pathName === "/product") {
-    // res.writeHead(200, { "Content-type": "text/html" });
-    // res.end(tempProduct);
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API Route
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
