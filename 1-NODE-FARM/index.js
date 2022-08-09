111954d453bc5a70d9ecd2ddfdf028fdf152dcd5
timzo31 <madty31@gmail.com>
2022-08-10 03:16:36 +0800

    1-NODE-FARM: HTML Templating: Parsing Variables From URLs, done.cd

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 //////////////////////////////////////
 //////////   SERVER    ///////////////
@@ -21,6 +23,11 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) =>
+  slugify(el.productName, { replacement: "-", lower: true })
+);
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
