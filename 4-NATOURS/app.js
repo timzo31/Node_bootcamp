492bce138c03cc4070f44baa6dd367bb27a5780b
timzo31 <madty31@gmail.com>
2022-08-22 05:15:44 +0800

    Sectiont 6:Natours, Handlig GET Requests, Done

--- 4-NATOURS/app.js
+++ 4-NATOURS/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const fs = require('fs');
+//const tours = require('./dev-data/data/tours-simple.json');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/tours', (req, res) => {
-  res.json({ message: 'Here are the available tours' });
+// app.post('/tours', (req, res) => {
+//   res.json({ message: 'Here are the available tours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
