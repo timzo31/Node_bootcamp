d589e50e505ed607a7eddbc8744e522084c9e302
timzo31 <madty31@gmail.com>
2022-08-22 06:00:16 +0800

    Sectiont 6:Natours, Handling POST Requests, Done

--- 4-NATOURS/app.js
+++ 4-NATOURS/app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -26,6 +28,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__filename}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send('Done');
 });
 
 const port = 3000;
