965cb07d5ebc3f78f8812b35d97d35cfac399c0a
timzo31 <madty31@gmail.com>
2022-08-24 02:16:08 +0800

    Sectiont 6:Natours, Environment Variables, Done

--- 4-NATOURS/app.js
+++ 4-NATOURS/app.js
@@ -7,14 +7,22 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
-
 // Using a middleware
-app.use(morgan('dev'));
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // 3) ROUTES MIDDLEWARES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use((req, res, next) => {
+  console.log('Hello from the Middleware');
+  next();
+});
 
 module.exports = app;
