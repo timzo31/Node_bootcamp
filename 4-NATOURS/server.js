e9f5e9807190a27c6bd5b95d1b9e40256fc61361
timzo31 <madty31@gmail.com>
2022-08-26 23:41:28 +0800

    Section 6: Mongo & Mongoose: Connection Our database with Express, Done

--- 4-NATOURS/server.js
+++ 4-NATOURS/server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+const DB =
+  'mongodb+srv://madty31:WawaD0uT00@natour-app.4yjnreo.mongodb.net/natours?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = require('./app');
 
