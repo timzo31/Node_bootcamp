0948c6a48701568be266447197d24d3b46fad556
timzo31 <madty31@gmail.com>
2022-08-28 23:50:54 +0800

    Section 6: MongoDB & Mongoose, Modelling the Tours, Done

--- 4-NATOURS/controllers/tourController.js
+++ 4-NATOURS/controllers/tourController.js
@@ -85,7 +85,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'Failure',
-      message: 'Failed to create a Tour !',
+      message: err.message,
     });
   }
 };
--- 4-NATOURS/models/tourModel.js
+++ 4-NATOURS/models/tourModel.js
@@ -5,15 +5,51 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A duration must be specified'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour should have a difficulty level'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startDate: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
