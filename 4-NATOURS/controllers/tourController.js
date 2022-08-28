221335ef27c197120efc6db96e5c7982216218d7
timzo31 <madty31@gmail.com>
2022-08-29 00:56:14 +0800

    Section 6: MongoDB & Mongoose, Making the API Better: Filtering, Done

--- 4-NATOURS/controllers/tourController.js
+++ 4-NATOURS/controllers/tourController.js
@@ -31,8 +31,25 @@ const { findById } = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(queryObj);
+
+    // const query= await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'Success',
       requestedAt: req.requestTime,
