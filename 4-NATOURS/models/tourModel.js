7ca4ac6b4978b9b48e23af730e1c808e3c05e4a7
timzo31 <madty31@gmail.com>
2022-09-02 22:23:49 +0800

    Section 6: MongoDB & Mongoose, Middlewares: Document & Query, Done

--- 4-NATOURS/models/tourModel.js
+++ 4-NATOURS/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+
+    slug: String,
+
     duration: {
       type: Number,
       required: [true, 'A duration must be specified'],
@@ -52,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -61,6 +69,37 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// // DOCUMENT MIDDLEWARE: runs before .save() & .create() commande
+// tourSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE:
+//tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} miliseconds !`);
+  console.log(docs);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
