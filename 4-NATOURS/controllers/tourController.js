5ee52934fe62b8d5fbd0034e7876c4c05e507185
timzo31 <madty31@gmail.com>
2022-09-02 20:05:27 +0800

    Section 6: MongoDB & Mngoose: Aggregation Pipeline: Matching & Grouping, Done

--- 4-NATOURS/controllers/tourController.js
+++ 4-NATOURS/controllers/tourController.js
@@ -118,3 +118,44 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          // _id: '$ratingAverage',
+          _id: { $toUpper: '$difficulty' },
+          numRatings: { $sum: '$ratingsQuantity' },
+          numTours: { $sum: 1 },
+          averageRating: { $avg: '$ratingAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'Success',
+      message: 'Tour successfully retrieved !!',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Failure',
+      message: err.message,
+    });
+  }
+};
