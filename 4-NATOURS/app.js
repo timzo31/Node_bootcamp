7f0bd3686882103ee2c42710e65fa5c8f3794b3f
timzo31 <69592997+timzo31@users.noreply.github.com>
2022-09-07 01:12:20 +0800

    Section 9: Error Handling with Express: Handling unhandled routes, Done

--- 4-NATOURS/app.js
+++ 4-NATOURS/app.js
@@ -10,19 +10,19 @@ const app = express();
 // Using a middleware
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
 // 3) ROUTES MIDDLEWARES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
-app.use((req, res, next) => {
-  console.log('Hello from the Middleware');
-  next();
-});
 
 module.exports = app;
